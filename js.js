286dffcd18fcf9458719a8dbc4c2eff24856e97b
Pablo <percara.pablo@gmail.com>
2023-01-24 14:08:49 -0300

    version 1

---
+++ js.js
@@ -0,0 +1,12 @@
+
+
+let btn_cuadrado = document.getElementById('btn-cuadrado')
+let btn_rectangulo = document.getElementById('btn-rectangulo')
+let btn_triangulo = document.getElementById('btn-triangulo')
+let btn_rombo = document.getElementById('btn-rombo')
+let btn_paralelo = document.getElementById('btn-paralelo')
+let btn_trapecio = document.getElementById('btn-trapecio')
+let btn_poligo = document.getElementById('btn-poligo')
+let btn_circulo = document.getElementById('btn-circulo')
+
+
